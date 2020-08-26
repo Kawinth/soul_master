8cf0e18906b939435fa0f5d0bdd0943d8b53dbbd
Kawinth <skw1915@163.com>
2020-08-26 22:29:32 +0800

    实现了摇杆

---
+++ lib/View.dart
@@ -0,0 +1,5 @@
+enum View {
+  entrypage,
+  maincity,
+  field,
+}
---
+++ lib/soul_master_game.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+import 'package:flame/game.dart';
+
+class SoulMasterGame extends Game {
+  Size screenSize;
+
+  @override
+  void render(Canvas c) {}
+
+  @override
+  void update(double t) {}
+
+  @override
+  void resize(Size size) {
+    screenSize = size;
+  }
+}
